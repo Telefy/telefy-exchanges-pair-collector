fd05df368d52839e2ce7b5045ed05fe3694757a2
sathishkumarsb <sathishsks1296@gmail.com>
2021-12-28 16:14:07 +0530

    binance based script changed

--- scriptNew.js
+++ scriptNew.js
@@ -20,7 +20,7 @@ con.connect(function (err) {
 
 const init = async ()=> {
         await con.query(
-          `SELECT * FROM m_exchanges where exchange_id !='3' ORDER by exchange_id ASC`,
+          `SELECT name as exchange_type,exchange_id FROM m_exchanges order by exchange_id desc limit 2`,
           async (err, exresult) => {
             if (err) throw err;
             for(let exchange=0; exchange < exresult.length; exchange++){
@@ -28,7 +28,7 @@ const init = async ()=> {
                 let waitLoop = new Promise(async (resolve,reject)=>{            
 
                     let otherExchanges = exresult.filter(function(element){            
-                        return element.exchange_id !== exresult[exchange].exchange_id;
+                        return element.exchange_type !== exresult[exchange].exchange_type;
                     });
 
                     let parameter = {
@@ -54,7 +54,7 @@ const init = async ()=> {
 const getBaseExchangePair = async (values, nextLoop, loopReject) => {
   let getAllUrlConfig = {
     method: "GET",
-    url: `http://localhost:5000/allPairs/${values.baseExchange.exchange_id}`,
+    url: `http://localhost:5000/allPairs/${values.baseExchange.exchange_type}`,
     headers: {
       "Content-Type": "application/json",
     },
@@ -62,7 +62,7 @@ const getBaseExchangePair = async (values, nextLoop, loopReject) => {
 
   axios(getAllUrlConfig).then(async (pairsResponse) => {
     if (pairsResponse.data.data.length > 0) {      
-        pairs.push({[`${values.baseExchange.name}`]:pairsResponse.data.data,exchange: values.baseExchange.name,exchange_id : values.baseExchange.exchange_id,checked:[]})
+        pairs.push({[`${values.baseExchange.exchange_type}`]:pairsResponse.data.data,exchange: values.baseExchange.exchange_type,exchange_id : values.baseExchange.exchange_id,checked:[]})
         nextLoop(0)  
     } else {
       nextLoop(0);
@@ -73,6 +73,7 @@ const getBaseExchangePair = async (values, nextLoop, loopReject) => {
 
 
 const checkCommonPairs = async () => {
+  console.log("-------common function ------")
   for (let i = 0; i < pairs.length; i++) {
     let baseExchange = pairs[i].exchange;
     let baseExchangeId = pairs[i].exchange_id;
@@ -109,19 +110,20 @@ const checkCommonPairs = async () => {
               dataInsert.push(post);
             } else {
               let [getIndexElse, otherPairIdElse] = await getLoopIndex(
-                baseToken0,
                 baseToken1,
+                baseToken0,
                 otherExName,
                 otherPairs
               );
               if (getIndexElse >= 0) {
                 let deleteArray = otherPairs.splice(getIndexElse, 1);
-                let post = `('${symbol}','${otherExchangeId}','${otherPairIdElse}','${baseToken0}','${baseToken1}','${decimal0}','${decimal1}'),('${symbol}','${baseExchangeId}','${pairId}','${baseToken0}','${baseToken1}','${decimal0}','${decimal1}')`;
+                let post = `('${symbol}','${otherExchangeId}','${otherPairIdElse}','${baseToken1}','${baseToken0}','${decimal0}','${decimal1}'),('${symbol}','${baseExchangeId}','${pairId}','${baseToken0}','${baseToken1}','${decimal0}','${decimal1}')`;
                 dataInsert.push(post);
               }
             }
           
             if (i == pairs.length -1 && j == exchangePairs.length - 1 && s == otherExchanges.length -1) {
+              console.log(dataInsert,"-------common function ------")
                 if(dataInsert.length > 0){
                   var post = dataInsert.join();
                   var sql = `INSERT INTO m_common_pair (symbol,exchange_id,pairtoken,token0,token1,decimal0,decimal1) values ${post}`;
@@ -146,31 +148,18 @@ const getObjectInfo = async (exchange,exchangeData)=> {
   let symbol;
   let decimal0;
   let decimal1;
-  if(exchange == "UNISWAP"){
+  if(exchange == "PANCAKESWAP"){
     token0 = exchangeData.token0.id
     token1 = exchangeData.token1.id
     decimal0 = exchangeData.token0.decimals
     decimal1 = exchangeData.token1.decimals
     pairId = exchangeData.id
     symbol = exchangeData.token0.symbol+"/"+exchangeData.token1.symbol;
-  } else if(exchange == "SUSHISWAP") {
+  } else if(exchange == "APESWAP") {
     token0 = exchangeData.token0.id
     token1 = exchangeData.token1.id
     decimal0 = exchangeData.token0.decimals
     decimal1 = exchangeData.token1.decimals
-    pairId = exchangeData.id
-    symbol = exchangeData.token0.symbol+"/"+exchangeData.token1.symbol;
-  } else if(exchange == "BANCOR") {
-    if(exchangeData.reserves.length >= 2){
-      token0 = exchangeData.reserves[0].dlt_id;
-      token1 = exchangeData.reserves[1].dlt_id;
-      pairId = exchangeData.dlt_id;
-      symbol = exchangeData.name;
-    }
-    
-  } else if(exchange == "BALANCER") {
-    token0 = exchangeData.token0.id
-    token1 = exchangeData.token1.id
     pairId = exchangeData.id
     symbol = exchangeData.token0.symbol+"/"+exchangeData.token1.symbol;
   } 
@@ -182,25 +171,16 @@ const getLoopIndex = async (token0,token1,exchange,otherPairs) => {
 
   let index;
   let pairId;
-  
-  if(exchange == "UNISWAP"){
-    index = otherPairs.findIndex((otherPair) => otherPair.token0.id === token0 && otherPair.token1.id === token1);
+  if(exchange == "PANCAKESWAP"){
+    index = otherPairs.findIndex((otherPair) => otherPair.token0.id.toLowerCase() === token0.toLowerCase() && otherPair.token1.id.toLowerCase() === token1.toLowerCase());
     if(index >= 0){      
       pairId = otherPairs[index].id
     }
-  } else if(exchange == "SUSHISWAP") {
-    index = otherPairs.findIndex((otherPair) => otherPair.token0.id === token0 && otherPair.token1.id === token1);
+  } else if(exchange == "APESWAP") {
+    index = otherPairs.findIndex((otherPair) => otherPair.token0.id.toLowerCase() === token0.toLowerCase() && otherPair.token1.id.toLowerCase() === token1.toLowerCase());
     if(index >= 0){
       pairId = otherPairs[index].id
     }
-  } else if(exchange == "BANCOR") {
-    index = otherPairs.findIndex((otherPair) => otherPair.reserves[0].dlt_id === token0 && otherPair.reserves[1].dlt_id === token1);
-    if(index >= 0){
-      pairId = otherPairs[index].dlt_id
-    }
-  } else if(exchange == "BALANCER") {
-    index = otherPairs.findIndex((otherPair) => otherPair.token0.id === token0 && otherPair.token1.id === token1);
-    pairId = otherPairs[index].id
   } 
 
   return [index,pairId]
